3db86bcffb0a888528f1e7b183b8f2776b2e90ba
Neil Vass <neil.vass+github@gmail.com>
2024-12-01 09:40:57 +0000

    Day 1 done

--- src/day01/day01.test.ts
+++ src/day01/day01.test.ts
@@ -1,8 +1,44 @@
 import {expect, describe, it} from "vitest";
-import {fn} from "./day01.js";
+import {countOccurrences, parseLine, solvePart1, solvePart2} from "./day01.js";
+import {Sequence} from "generator-sequences";
 
-describe("#fn", () => {
-    it("runs", () => {
-        expect(fn("")).toBe("a real test");
+describe("Part 1", () => {
+    it("Parses line", () => {
+        expect(parseLine("3   4")).toStrictEqual([3, 4]);
+    });
+
+    it("Solves example", async () => {
+        const input = new Sequence([
+            "3   4",
+            "4   3",
+            "2   5",
+            "1   3",
+            "3   9",
+            "3   3",
+        ]);
+        expect(await solvePart1(input)).toBe(11);
+    });
+});
+
+describe("Part 2", () => {
+    it("Counts occurrences", () => {
+        const list = [4, 3, 5, 3, 3];
+        expect(countOccurrences(list)).toStrictEqual({
+            4: 1,
+            3: 3,
+            5: 1
+        });
+    });
+
+    it("Solves example", async () => {
+        const input = new Sequence([
+            "3   4",
+            "4   3",
+            "2   5",
+            "1   3",
+            "3   9",
+            "3   3",
+        ]);
+        expect(await solvePart2(input)).toBe(31);
     });
 });
--- src/day01/day01.ts
+++ src/day01/day01.ts
@@ -1,9 +1,51 @@
-export function fn(filepath: string) {
-    return "Hello, World!";
+import {linesFromFile, Sequence} from "generator-sequences";
+
+export function parseLine(line: string) {
+    const m = line.match(/^(\d+)\s+(\d+)$/);
+    if (!m) throw new Error(`Unrecognized format: ${line}`);
+    return [+m[1], +m[2]];
+}
+
+async function createLists(input: Sequence<string>) {
+    const list1 = new Array<number>();
+    const list2 = new Array<number>();
+
+    for await (const line of input) {
+        const [left, right] = parseLine(line);
+        list1.push(left);
+        list2.push(right);
+    }
+    return {list1, list2};
+}
+
+export async function solvePart1(input: Sequence<string>) {
+    const {list1, list2} = await createLists(input);
+
+    list1.sort((a, b) => a-b);
+    list2.sort((a, b) => a-b);
+
+    let sumOfDiffs = 0;
+    for (let i = 0; i < list1.length; i++) {
+        sumOfDiffs += Math.abs(list1[i] - list2[i]);
+    }
+    return sumOfDiffs;
+}
+
+export function countOccurrences(list: number[]) {
+    const counter: { [key: number]: number } = {};
+    list.forEach(val => counter[val] ? counter[val]++ : counter[val] = 1);
+    return counter;
+}
+
+export async function solvePart2(input: Sequence<string>) {
+    const {list1, list2} = await createLists(input);
+    const occurrences = countOccurrences(list2);
+    return list1.reduce((acc, val) => (acc += val * occurrences[val] || 0), 0);
 }
 
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
     const filepath = `${import.meta.dirname}/day01.input.txt`;
-    console.log(fn(filepath));
+    const input = linesFromFile(filepath);
+    console.log(await solvePart2(input));
 }
