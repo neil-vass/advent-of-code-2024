bec4f817267eadb06dd904463e6053305fd7fee3
Neil Vass <neil.vass+github@gmail.com>
2024-12-03 06:15:20 +0000

    Day 3 done

--- src/day03/day03.test.ts
+++ src/day03/day03.test.ts
@@ -1,9 +1,16 @@
 import {describe, expect, it} from "vitest";
-import {sumOfMuls} from "./day03.js";
+import {sumOfMuls, sumOfMulsWithConditionals} from "./day03.js";
 
 describe("Part 1", () => {
     it("Solves example", () => {
         const line = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))";
         expect(sumOfMuls(line)).toBe(161);
+    });
+});
+
+describe("Part 2", () => {
+    it("Solves example", () => {
+        const line = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))";
+        expect(sumOfMulsWithConditionals(line)).toBe(48);
     });
 });
--- src/day03/day03.ts
+++ src/day03/day03.ts
@@ -9,12 +9,36 @@ export function sumOfMuls(line: string) {
     return sum;
 }
 
+export function sumOfMulsWithConditionals(line: string) {
+    const matches = line.matchAll(/(mul\((\d+),(\d+)\))|(do\(\))|(don't\(\))/g);
+    let sum = 0;
+    let process = true;
+    for (const m of matches) {
+        if (m[0] === "do()") {
+            process = true;
+        } else if (m[0] === "don't()") {
+            process = false;
+        }
+        else if (m[0].startsWith("mul") && process){
+            sum += +m[2] * +m[3];
+        }
+    }
+    return sum;
+
+}
+
 export async function solvePart1(lines: Sequence<string>) {
     return await Sequence.sum(lines.map(sumOfMuls));
+}
+
+export async function solvePart2(lines: Sequence<string>) {
+    const oneLine = await lines.reduce((acc, val) => acc + val, "");
+    return sumOfMulsWithConditionals(oneLine);
 }
 
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
     const filepath = `${import.meta.dirname}/day03.input.txt`;
-    console.log(await solvePart1(linesFromFile(filepath)));
+    console.log(await solvePart2(linesFromFile(filepath)));
 }
+
