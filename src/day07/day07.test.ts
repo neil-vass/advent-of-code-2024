06917fb3d5b89dfba2eed9b1ec85dae611b84df5
Neil Vass <neil.vass+github@gmail.com>
2024-12-07 10:02:41 +0000

    Day 7 done

---
+++ src/day07/day07.test.ts
@@ -0,0 +1,52 @@
+import {describe, expect, it} from "vitest";
+import {add, con, couldBeTrue, mul, parseLine, solvePart1, solvePart2} from "./day07.js";
+import {Sequence} from "generator-sequences";
+
+describe("Part 1", () => {
+    it("Parses lines", () => {
+        expect(parseLine("190: 10 19")).toStrictEqual([190, [10,19]]);
+    });
+
+    it("Checks single equations", () => {
+        expect(couldBeTrue(190, [10,19])).toBe(true);
+        expect(couldBeTrue(3267, [81, 40, 27])).toBe(true);
+        expect(couldBeTrue(83, [17, 5])).toBe(false);
+    });
+
+    it("Solves example", async () => {
+        const lines = new Sequence([
+            "190: 10 19",
+            "3267: 81 40 27",
+            "83: 17 5",
+            "156: 15 6",
+            "7290: 6 8 6 15",
+            "161011: 16 10 13",
+            "192: 17 8 14",
+            "21037: 9 7 18 13",
+            "292: 11 6 16 20",
+        ]);
+        expect(await solvePart1(lines)).toBe(3749);
+    });
+});
+
+describe("Part 2", () => {
+    it("Applies concatenation", () => {
+        const ops = [add, mul, con];
+        expect(couldBeTrue(156, [15, 6], ops)).toBe(true);
+    });
+
+    it("Solves example", async () => {
+        const lines = new Sequence([
+            "190: 10 19",
+            "3267: 81 40 27",
+            "83: 17 5",
+            "156: 15 6",
+            "7290: 6 8 6 15",
+            "161011: 16 10 13",
+            "192: 17 8 14",
+            "21037: 9 7 18 13",
+            "292: 11 6 16 20",
+        ]);
+        expect(await solvePart2(lines)).toBe(11387);
+    });
+});
---
+++ src/day07/day07.ts
@@ -0,0 +1,53 @@
+import {linesFromFile, Sequence} from "generator-sequences";
+
+export function parseLine(line: string): [number, number[]] {
+    if (!line.match(/^\d+:( \d+)+$/)) throw new Error(`Bad format`);
+    const [testVal, rest] = line.split(": ");
+    const equationVals = rest.split(" ");
+    return [Number(testVal), equationVals.map(Number)];
+}
+
+export const add = (x: number, y: number) => x+y;
+export const mul = (x: number, y: number) => x*y;
+export const con = (x: number, y: number) => +(""+x+y);
+
+export function couldBeTrue(testVal: number, equationVals: number[], operators=[add, mul]) {
+
+    function hasSolution(head: number, rest: number[]): boolean {
+        if (rest.length === 0) {
+            return head === testVal;
+        } else {
+            return operators.some(op => hasSolution(op(head, rest[0]), rest.slice(1)));
+        }
+    }
+
+    return hasSolution(equationVals[0], equationVals.slice(1));
+}
+
+export async function solvePart1(lines: Sequence<string>) {
+    let calibrationResult = 0;
+    for await (const [testVal, equationVals] of lines.map(parseLine)) {
+        if (couldBeTrue(testVal, equationVals)) {
+            calibrationResult += testVal;
+        }
+    }
+    return calibrationResult;
+}
+
+export async function solvePart2(lines: Sequence<string>) {
+    const operators = [add, mul, con];
+    let calibrationResult = 0;
+    for await (const [testVal, equationVals] of lines.map(parseLine)) {
+        if (couldBeTrue(testVal, equationVals, operators)) {
+            calibrationResult += testVal;
+        }
+    }
+    return calibrationResult;
+}
+
+
+// If this script was invoked directly on the command line:
+if (`file://${process.argv[1]}` === import.meta.url) {
+    const filepath = `${import.meta.dirname}/day07.input.txt`;
+    console.log(await solvePart2(linesFromFile(filepath)));
+}
