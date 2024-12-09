7756ab1106f8df83ee9c613cba0be73e77f5ab7a
Neil Vass <neil.vass+github@gmail.com>
2024-12-09 23:42:48 +0000

    Day 9 done

--- src/day09/day09.test.ts
+++ src/day09/day09.test.ts
@@ -1,10 +1,23 @@
 import {describe, expect, it} from "vitest";
-import {checksumFor} from "./day09.js";
+import {checksumFor, checksumForV2} from "./day09.js";
 
 
 describe("Part 1", () => {
     it("Solves example", async () => {
         const diskMap = "2333133121414131402";
         expect(checksumFor(diskMap)).toBe(1928);
+    });
+});
+
+
+describe("Part 2", () => {
+    it("Solves example", async () => {
+        const diskMap = "2333133121414131402";
+        expect(checksumForV2(diskMap)).toBe(2858);
+    });
+
+    it("Edge case", async () => {
+        const diskMap = "12345";
+        expect(checksumForV2(diskMap)).toBe(132);
     });
 });
--- src/day09/day09.ts
+++ src/day09/day09.ts
@@ -1,15 +1,20 @@
 import {singleLineFromFile} from "generator-sequences";
 
-type File = { id: number, length: number };
+type FileInfo = { id: number, length: number };
 
-export function checksumFor(diskMap: string) {
-    const files = new Array<File>();
+function findFilesAndGaps(diskMap: string) {
+    const files = new Array<FileInfo>();
     const gaps = new Array<number>();
     for (let i = 0; i < diskMap.length; i += 2) {
         files.push({id: i / 2, length: +diskMap[i]});
         if (i + 1 === diskMap.length) break;
         gaps.push(+diskMap[i + 1]);
     }
+    return {files, gaps};
+}
+
+export function checksumFor(diskMap: string) {
+    const {files, gaps} = findFilesAndGaps(diskMap);
 
     let outputIdx = 0;
     let checksum = 0;
@@ -45,8 +50,58 @@ export function checksumFor(diskMap: string) {
     return checksum;
 }
 
+type ExtendedFileInfo = FileInfo & {initialIdx: number};
+type GapInfo = { initialIdx: number, length: number };
+
+function findFilesAndGapsV2(diskMap: string) {
+    const files = new Array<ExtendedFileInfo>();
+    const gaps = new Array<GapInfo>();
+    let position = 0;
+    for (let i = 0; i < diskMap.length; i += 2) {
+        const fileLength = +diskMap[i];
+        files.push({id: i / 2, length: fileLength, initialIdx: position});
+        position += fileLength;
+
+        if (i+1 === diskMap.length) break;
+        const gapLength = +diskMap[i+1]
+        gaps.push({initialIdx: position, length: gapLength});
+        position += gapLength;
+    }
+    return {files, gaps};
+}
+
+export function checksumForV2(diskMap: string) {
+    const {files, gaps} = findFilesAndGapsV2(diskMap);
+    const unmovedFiles = [files[0]];
+
+    let checksum = 0;
+    for (let fileIdx = files.length-1; fileIdx > 0; fileIdx--) {
+        const file = files[fileIdx];
+        const gapIdx = gaps.findIndex(g => g.length >= file.length && g.initialIdx < file.initialIdx);
+        if(gapIdx === -1) {
+            unmovedFiles.push(file);
+            continue;
+        }
+        const gap = gaps[gapIdx];
+        for (let blockNum = 0; blockNum < file.length; blockNum++) {
+            checksum += gap.initialIdx * file.id;
+            gap.initialIdx++;
+            gap.length--;
+        }
+    }
+
+    for (const file of unmovedFiles) {
+        for (let blockNum = 0; blockNum < file.length; blockNum++) {
+            checksum += file.initialIdx * file.id;
+            file.initialIdx++;
+        }
+    }
+    return checksum;
+}
+
+
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
     const filepath = `${import.meta.dirname}/day09.input.txt`;
-    console.log(checksumFor(singleLineFromFile(filepath)));
+    console.log(checksumForV2(singleLineFromFile(filepath)));
 }
