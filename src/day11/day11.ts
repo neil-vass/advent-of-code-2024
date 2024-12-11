e5b9c92af9fa1d4bcea5dc03092c106110fcd64a
Neil Vass <neil.vass+github@gmail.com>
2024-12-11 17:06:04 +0000

    Use a wrapper function

--- src/day11/day11.ts
+++ src/day11/day11.ts
@@ -13,23 +13,15 @@ export function singleStoneBlink(stone: number): number[] {
     return [stone * 2024];
 }
 
-const cache = new Map<string, number>();
-
 // After multiple blinks, how many stones do we end up with from this single stone?
-export function stoneCountFromSingleStone(stone: number, blinks: number): number {
-    let result = cache.get(JSON.stringify([stone, blinks]));
-    if (result === undefined) {
-
-        const stonesAfterThisBlink = singleStoneBlink(stone);
-        if (blinks === 1) {
-            result = stonesAfterThisBlink.length;
-        } else {
-            result = stoneCountFromStoneCollection(stonesAfterThisBlink, blinks-1);
-        }
-
-        cache.set(JSON.stringify([stone, blinks]), result);
+// We're going to wrap this so we rememberResults of all calls, so it's declared with "let".
+export let stoneCountFromSingleStone = function(stone: number, blinks: number): number {
+    const stonesAfterThisBlink = singleStoneBlink(stone);
+    if (blinks === 1) {
+        return stonesAfterThisBlink.length;
+    } else {
+        return stoneCountFromStoneCollection(stonesAfterThisBlink, blinks-1);
     }
-    return result;
 }
 
 // After multiple blinks, how many stones do we end up with from this collection?
@@ -37,10 +29,30 @@ export function stoneCountFromStoneCollection(stones: number[], blinks: number):
     return stones.reduce((acc, val) => acc + stoneCountFromSingleStone(val, blinks), 0);
 }
 
+
+// The above all takes too long, let's remember the answer every time we do
+// a stone count - the same ones come up repeatedly.
+const cache = new Map<string, any>();
+
+function rememberResults(fn: any) {
+    return function() {
+        let result = cache.get(JSON.stringify(arguments));
+        if (result === undefined) {
+            result = fn.apply(undefined, arguments);
+            cache.set(JSON.stringify(arguments), result);
+        }
+        return result;
+    }
+}
+
+stoneCountFromSingleStone = rememberResults(stoneCountFromSingleStone);
+
+
 export function solvePart1(line: string, blinks: number) {
     const stones = line.split(" ").map(Number);
     return stoneCountFromStoneCollection(stones, blinks);
 }
+
 
 // If this script was invoked directly on the command line:
 if (`file://${process.argv[1]}` === import.meta.url) {
