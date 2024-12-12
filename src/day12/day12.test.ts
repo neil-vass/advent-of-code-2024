d843052a1adcd1584c86caf432d3da905d7d3d6e
Neil Vass <neil.vass+github@gmail.com>
2024-12-12 08:23:39 +0000

    Day 12 first draft - incorrect for multiple regions of same crop

--- src/day12/day12.test.ts
+++ src/day12/day12.test.ts
@@ -2,9 +2,26 @@ import {expect, describe, it} from "vitest";
 import {solvePart1} from "./day12.js";
 import {Sequence} from "generator-sequences";
 
-describe("Part 1", () => {
-    it("Solves example", async () => {
-        const lines = new Sequence([]);
-        expect(await solvePart1(lines)).toBe("a real test");
+describe("Part 2", () => {
+    it("Solves small example", async () => {
+        const lines = new Sequence([
+            "AAAA",
+            "BBCD",
+            "BBCC",
+            "EEEC",
+        ]);
+        expect(await solvePart1(lines)).toBe(140);
     });
+
+    it("Solves for enclosed regions", async () => {
+        const lines = new Sequence([
+            "OOOOO",
+            "OXOXO",
+            "OOOOO",
+            "OXOXO",
+            "OOOOO",
+        ]);
+        expect(await solvePart1(lines)).toBe(772);
+    });
+
 });
--- src/day12/day12.ts
+++ src/day12/day12.ts
@@ -1,7 +1,58 @@
 import {linesFromFile, Sequence} from "generator-sequences";
 
+
+export class Garden {
+    private constructor(readonly plots: string[]) {}
+
+    static async buildFromDescription(lines: Sequence<string>) {
+        const plots = await lines.toArray();
+        return new Garden(plots);
+    }
+
+    isInBounds(row: number, col: number) {
+        return row >= 0 && col >= 0 &&
+            row < this.plots.length &&
+            col < this.plots[0].length;
+    }
+
+    fenceForPlot(row: number, col: number) {
+        const neighbours = [
+            [row-1, col], [row+1, col], [row, col-1], [row, col+1]
+        ]
+        let fences = 4;
+        for (const [nRow, nCol] of neighbours) {
+             if (this.isInBounds(nRow, nCol) &&
+                this.plots[nRow][nCol] === this.plots[row][col]) {
+                    fences--;
+            }
+        }
+        return fences;
+    }
+
+    totalFenceCost() {
+        type Region = { area: number, perimeter: number }
+        const cropDetails = new Map<string, Region>();
+
+        for (let row = 0; row < this.plots.length; row++) {
+            for (let col = 0; col < this.plots[0].length; col++) {
+                const crop = this.plots[row][col];
+                let details = cropDetails.get(crop);
+                if (details === undefined) {
+                    details = { area: 0, perimeter: 0};
+                    cropDetails.set(crop, details);
+                }
+                details.area++;
+                details.perimeter += this.fenceForPlot(row, col);
+            }
+        }
+
+        return [...cropDetails.values()].reduce((acc, val) => acc + val.area * val.perimeter, 0);
+    }
+}
+
 export async function solvePart1(lines: Sequence<string>) {
-    return "Hello, World!";
+    const garden = await Garden.buildFromDescription(lines);
+    return garden.totalFenceCost();
 }
 
 // If this script was invoked directly on the command line:
