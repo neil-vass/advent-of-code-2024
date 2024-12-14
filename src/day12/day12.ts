3f233b633c55cf9527185400be6543de396b0a29
Neil Vass <neil.vass+github@gmail.com>
2024-12-14 16:59:41 +0000

    Day 12 don't track sides

--- src/day12/day12.ts
+++ src/day12/day12.ts
@@ -1,6 +1,6 @@
 import {linesFromFile, Sequence} from "generator-sequences";
 
-type Region = { area: number, perimeter: number, sides: number };
+type Region = { area: number, perimeter: number };
 
 export class Garden {
     private constructor(readonly plots: string[]) {}
@@ -16,15 +16,11 @@ export class Garden {
             col < this.plots[0].length;
     }
 
-    exploreRegion(row: number, col: number, reached: Set<string>, fencesOnNeighbours: Set<string>[][]|null=null): Region {
+    exploreRegion(row: number, col: number, reached: Set<string>, fences: Set<string>[][]): Region {
         if (reached.has(JSON.stringify([row, col]))) {
-            return {area: 0, perimeter: 0, sides: 0};
+            return {area: 0, perimeter: 0};
         }
         reached.add(JSON.stringify([row, col]));
-
-        if (fencesOnNeighbours === null) {
-            fencesOnNeighbours = Array.from(this.plots, r => Array.from(this.plots[0], c => new Set<string>()));
-        }
 
         const neighbours = [
             {dir: "^", nRow: row-1, nCol: col },
@@ -33,45 +29,40 @@ export class Garden {
             {dir: ">", nRow: row, nCol: col+1 },
         ];
         const toExplore = new Array<{nRow: number, nCol: number}>();
-        let fences = new Set<string>();
+        let fencesOnThisPlot = new Set<string>();
         for (const {dir, nRow, nCol} of neighbours) {
              if (this.isInBounds(nRow, nCol) && this.plots[nRow][nCol] === this.plots[row][col]) {
                  toExplore.push({nRow, nCol});
             } else {
-                 fences.add(dir);
+                 fencesOnThisPlot.add(dir);
             }
         }
 
         let area = 1;
-        let perimeter = fences.size;
-        let sides = 0;
-        let sidesNotCountedByNeighbours = fences;
+        let perimeter = fencesOnThisPlot.size;
         for (const n of toExplore) {
-            const details = this.exploreRegion(n.nRow, n.nCol, reached, fencesOnNeighbours);
+            const details = this.exploreRegion(n.nRow, n.nCol, reached, fences);
             area += details.area;
             perimeter += details.perimeter;
-            sides += details.sides;
-            fencesOnNeighbours[row][col] = fencesOnNeighbours[row][col].union(fences.intersection(fencesOnNeighbours[n.nRow][n.nCol]));
-            sidesNotCountedByNeighbours = sidesNotCountedByNeighbours.difference(fencesOnNeighbours[n.nRow][n.nCol]);
         }
-        sides += sidesNotCountedByNeighbours.size;
-        fencesOnNeighbours[row][col] = fences;
+        fences[row][col] = fencesOnThisPlot;
 
-        return {area, perimeter, sides};
+        return {area, perimeter};
     }
 
     totalFenceCost(costCalculator: (r: Region) => number) {
         const reached = new Set<string>();
+        const fences = Array.from(this.plots, r => Array.from(this.plots[0], c => new Set<string>()));
         let fenceCost = 0;
 
         for (let row = 0; row < this.plots.length; row++) {
             for (let col = 0; col < this.plots[0].length; col++) {
-                const region = this.exploreRegion(row, col, reached);
+                const region = this.exploreRegion(row, col, reached, fences);
                 if (region.area !== 0) console.log(this.plots[row][col], region)
                 fenceCost += costCalculator(region);
             }
         }
-
+        console.log(fences)
         return fenceCost;
     }
 }
@@ -85,7 +76,7 @@ export async function solvePart1(lines: Sequence<string>) {
 
 export async function solvePart2(lines: Sequence<string>) {
     const garden = await Garden.buildFromDescription(lines);
-    const costCalculator = (r: Region) => r.area * r.sides;
+    const costCalculator = (r: Region) => 1;
     return garden.totalFenceCost(costCalculator);
 }
 
