3e924695e8ceeef7348551427f9ec4ae2dc7cff7
Neil Vass <neil.vass+github@gmail.com>
2024-12-15 23:06:03 +0000

    Day 13 parsing

--- src/day13/day13.test.ts
+++ src/day13/day13.test.ts
@@ -1,10 +1,37 @@
 import {expect, describe, it} from "vitest";
-import {solvePart1} from "./day13.js";
+import {Machine, solvePart1} from "./day13.js";
 import {Sequence} from "generator-sequences";
 
 describe("Part 1", () => {
+    it("Creates machine", () => {
+        const lines = [
+            "Button A: X+94, Y+34",
+            "Button B: X+22, Y+67",
+            "Prize: X=8400, Y=5400",
+        ];
+        const machine = Machine.buildFromDescription(lines);
+        expect(machine.canWin()).toBe(true);
+        expect(machine.cheapestWin()).toBe(280);
+    });
+
     it("Solves example", async () => {
-        const lines = new Sequence([]);
-        expect(await solvePart1(lines)).toBe("a real test");
+        const lines = new Sequence([
+            "Button A: X+94, Y+34",
+            "Button B: X+22, Y+67",
+            "Prize: X=8400, Y=5400",
+            "",
+            "Button A: X+26, Y+66",
+            "Button B: X+67, Y+21",
+            "Prize: X=12748, Y=12176",
+            "",
+            "Button A: X+17, Y+86",
+            "Button B: X+84, Y+37",
+            "Prize: X=7870, Y=6450",
+            "",
+            "Button A: X+69, Y+23",
+            "Button B: X+27, Y+71",
+            "Prize: X=18641, Y=10279",
+        ]);
+        expect(await solvePart1(lines)).toBe(480);
     });
 });
--- src/day13/day13.ts
+++ src/day13/day13.ts
@@ -1,5 +1,38 @@
 import {linesFromFile, Sequence} from "generator-sequences";
 
+export type XY = {x: number, y: number};
+
+export class Machine {
+    private constructor(private readonly aMove: XY,
+                        private readonly bMove: XY,
+                        private readonly prize: XY) {}
+
+    static buildFromDescription(lines: string[]) {
+        const up = new Error(`Unexpected format: ${lines}`);
+
+        const aMatch = lines[0].match(/^Button A: X\+(\d+), Y\+(\d+)$/);
+        if (!aMatch) throw up;
+        const aMove = {x: +aMatch[1], y: +aMatch[2]};
+
+        const bMatch = lines[1].match(/^Button B: X\+(\d+), Y\+(\d+)$/);
+        if (!bMatch) throw up;
+        const bMove = {x: +bMatch[1], y: +bMatch[2]};
+
+        const prizeMatch = lines[2].match(/^Prize: X=(\d+), Y=(\d+)$/);
+        if (!prizeMatch) throw up;
+        const prize = {x: +prizeMatch[1], y: +prizeMatch[2]};
+
+        return new Machine(aMove, bMove, prize);
+    }
+
+    canWin(): any {
+        throw new Error("Method not implemented.");
+    }
+
+    cheapestWin(): any {
+        throw new Error("Method not implemented.");
+    }
+}
 export async function solvePart1(lines: Sequence<string>) {
     return "Hello, World!";
 }
