7666d230f0235b7fe98233e505d0dd3fa61f1853
Neil Vass <neil.vass+github@gmail.com>
2024-12-17 19:25:27 +0000

    Day17 part 1 done

---
+++ src/day17/day17.test.ts
@@ -0,0 +1,12 @@
+import {expect, describe, it} from "vitest";
+import {solvePart1} from "./day17.js";
+import {Sequence} from "generator-sequences";
+
+describe("Part 1", () => {
+    it("Solves example", async () => {
+        const lines = new Sequence([
+
+        ]);
+        expect(await solvePart1(lines)).toBe("a real test");
+    });
+});
